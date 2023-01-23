ae22a62a3b92cc77d53ddf590db9a292c5f416e4
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-23 14:14:00 +0200

    Fix attempt 1

--- _printf.c
+++ _printf.c
@@ -1,18 +1,30 @@
 #include "main.h"
 
 void print_buff(char buffer[], int *buff_ind);
+/**
+ * print_buff - Prints stuff in a buffer, if there's anything
+ * @buffer: Array of chars to print
+ * @buff_ind: Index to add preceeding chars, precisely the length
+ */
+void print_buff(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
 
 /**
  * _printf - Revised printf function
- * @format: Format of object to be printed
- * Return: Number of printed chars
+ * @format: format of objects to print
+ * Return: Number of chars printed
  */
 int _printf(const char *format, ...)
 {
-	int index, shown = 0, shown_char = 0;
+	int i, shown = 0, shown_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
@@ -21,17 +33,38 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(list, format);
-	for (index = 0; format[index] && format != '\0'; index++)
-	{
-		if (format[index] != '%')
-		{
-			buffer[buff_ind++] = format[index];
-			if (buff_ind == BUFF_SIZE)
-			{
-				print_buff(buffer, &buff_ind);
-				shown_char++;
-			}
-		}
-	}
-}
 
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		/* Task 0 */
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buff(buffer, &buff_ind);
+			shown_chars++;
+		}
+/*
+*		else
+*		{
+*			print_buff(buffer, &buff_ind);
+*			flags = get_flags(format, &i);
+*			width = get_width(format, &i, list);
+*			precision = get_precision(format, &i, list);
+*			size = get_size(format, &i);
+*			++i;
+*			shown = handle_print(format, &i, list, buffer,
+*				flags, width, precision, size);
+*			if (shown == -1)
+*				return (-1);
+*			shown_chars += shown;
+*		}
+*/
+	}
+
+/*	print_buff(buffer, &buff_ind);*/
+
+	va_end(list);
+
+	return (shown_chars);
+}
