dfabc75ad83bdb5dffb06bde6d315c7d5a9642a8
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-23 13:04:20 +0200

    Create main.h and task 1 file

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - Revised printf function
+ * @format: Format of object to be printed
+ * Return: Number of printed chars
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**Functions**/
+int _printf(const char *format, ...);
+
+#endif
