62dbae6b8bc975cdadf065003b62e0a53f01b0a4
tinashelorenzi@protonmail.com <tinashelorenzi@protonmail.com>
2023-01-23 14:18:12 +0200

    Shift void function

--- _printf.c
+++ _printf.c
@@ -1,18 +1,7 @@
 #include "main.h"
 
 void print_buff(char buffer[], int *buff_ind);
-/**
- * print_buff - Prints stuff in a buffer, if there's anything
- * @buffer: Array of chars to print
- * @buff_ind: Index to add preceeding chars, precisely the length
- */
-void print_buff(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
 
-	*buff_ind = 0;
-}
 
 
 /**
@@ -67,4 +56,17 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (shown_chars);
+}
+
+/**
+ * print_buff - Prints stuff in a buffer, if there's anything
+ * @buffer: Array of chars to print
+ * @buff_ind: Index to add preceeding chars, precisely the length
+ */
+void print_buff(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
